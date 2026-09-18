a100d369c3e4be9d0398c419acc34c3e641b33fa
BrendrWood <brendrwood@gmail.com>
2026-09-18 12:26:50 +0300

    Add base path for production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,15 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/base/',
   plugins: [react()],
+  server: {
+    proxy: {
+      '/base/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/base\/api/, '/api'),
+      },
+    },
+  },
 })
